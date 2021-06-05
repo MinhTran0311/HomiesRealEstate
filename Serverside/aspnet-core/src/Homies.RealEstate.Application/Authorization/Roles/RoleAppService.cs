929679c06be8d37499c2affbb5a36d4bdce70f54
MinhTran0311 <50918867+MinhTran0311@users.noreply.github.com>
2021-06-06 00:14:59 +0700

    get role

--- Serverside/aspnet-core/src/Homies.RealEstate.Application/Authorization/Roles/RoleAppService.cs
+++ Serverside/aspnet-core/src/Homies.RealEstate.Application/Authorization/Roles/RoleAppService.cs
@@ -10,24 +10,31 @@ using Microsoft.EntityFrameworkCore;
 using Homies.RealEstate.Authorization.Permissions;
 using Homies.RealEstate.Authorization.Permissions.Dto;
 using Homies.RealEstate.Authorization.Roles.Dto;
+using Homies.RealEstate.Authorization.Users.Profile.Dto;
+using Homies.RealEstate.Authorization.Users.Dto;
+using Abp.Domain.Repositories;
+using Abp.Authorization.Users;
 
 namespace Homies.RealEstate.Authorization.Roles
 {
     /// <summary>
     /// Application service that is used by 'role management' page.
     /// </summary>
-    [AbpAuthorize(AppPermissions.Pages_Administration_Roles)]
+    [AbpAuthorize]
     public class RoleAppService : RealEstateAppServiceBase, IRoleAppService
     {
         private readonly RoleManager _roleManager;
         private readonly IRoleManagementConfig _roleManagementConfig;
+        private readonly IRepository<UserRole, long> _userRoleRepository;
 
         public RoleAppService(
             RoleManager roleManager,
-            IRoleManagementConfig roleManagementConfig)
+            IRoleManagementConfig roleManagementConfig,
+            IRepository<UserRole, long> userRoleRepository)
         {
             _roleManager = roleManager;
             _roleManagementConfig = roleManagementConfig;
+            _userRoleRepository = userRoleRepository;
         }
 
         [HttpPost]
@@ -85,6 +92,93 @@ namespace Homies.RealEstate.Authorization.Roles
             };
         }
 
+        public async Task<GetRoleForViewOutput> GetRoleForView(NullableIdDto input)
+        {
+            var user = await GetCurrentUserAsync();
+            var userRole = await GetCurrentUserRole();
+
+            var permissions = PermissionManager.GetAllPermissions();
+            var grantedPermissions = new Permission[0];
+
+            var grantedPermission = new List<Permission>();
+            //var role = await _roleManager.GetRoleByIdAsync(userRole[0].RoleId);
+            //grantedPermissions = (await _roleManager.GetGrantedPermissionsAsync(role)).ToArray();
+
+            for (int i = 0; i < userRole.Count; i++)
+            {
+                var idvRole = await _roleManager.GetRoleByIdAsync(userRole[i].RoleId);
+                
+                grantedPermission.AddRange((List<Permission>)await _roleManager.GetGrantedPermissionsAsync(idvRole));
+            }
+            grantedPermissions = grantedPermission.ToArray();
+            return new GetRoleForViewOutput
+            {
+                Role = userRole,
+                //Permissions = ObjectMapper.Map<List<FlatPermissionDto>>(permissions).OrderBy(p => p.DisplayName).ToList(),
+                GrantedPermissionNames = grantedPermissions.Select(p => p.Name).ToList()
+            };
+        }
+
+        //Get Role of current user
+        private async Task<CurrentUserProfileEditDto> GetCurrentUserProfileForEdit()
+        {
+            var user = await GetCurrentUserAsync();
+
+            var userProfileEditDto = ObjectMapper.Map<CurrentUserProfileEditDto>(user);
+
+            userProfileEditDto.UserId = user.Id;
+
+            await FillRoleNames(userProfileEditDto);
+
+
+            return userProfileEditDto;
+        }
+
+        private async Task<List<UserListRoleDto>> GetCurrentUserRole()
+        {
+            var userProfile = await GetCurrentUserProfileForEdit();
+            return userProfile.Roles;
+        }
+
+        private async Task FillRoleNames(CurrentUserProfileEditDto userListDto)
+        {
+            /* This method is optimized to fill role names to given list. */
+            var userIds = userListDto.UserId;
+
+            var userRoles = await _userRoleRepository.GetAll()
+                .Where(userRole => userIds.Equals(userRole.UserId))
+                .Select(userRole => userRole).ToListAsync();
+
+            var distinctRoleIds = userRoles.Select(userRole => userRole.RoleId).Distinct();
+
+            var rolesOfUser = userRoles.Where(userRole => userRole.UserId == userListDto.UserId).ToList();
+            userListDto.Roles = ObjectMapper.Map<List<UserListRoleDto>>(rolesOfUser);
+
+            var roleNames = new Dictionary<int, string>();
+            var roleDisplayNames = new Dictionary<int, string>();
+
+            foreach (var roleId in distinctRoleIds)
+            {
+                var role = await _roleManager.FindByIdAsync(roleId.ToString());
+                if (role != null)
+                {
+                    roleNames[roleId] = role.Name;
+                    roleDisplayNames[roleId] = role.DisplayName;
+
+                }
+            }
+
+            foreach (var userListRoleDto in userListDto.Roles)
+            {
+                if (roleNames.ContainsKey(userListRoleDto.RoleId))
+                {
+                    userListRoleDto.RoleName = roleNames[userListRoleDto.RoleId];
+                    userListRoleDto.RoleDisplayName = roleDisplayNames[userListRoleDto.RoleId];
+                }
+            }
+            userListDto.Roles = userListDto.Roles.Where(r => r.RoleName != null).OrderBy(r => r.RoleName).ToList();
+        }
+        [AbpAuthorize(AppPermissions.Pages_Administration_Roles)]
         public async Task CreateOrUpdateRole(CreateOrUpdateRoleInput input)
         {
             if (input.Role.Id.HasValue)
@@ -119,6 +213,7 @@ namespace Homies.RealEstate.Authorization.Roles
             var role = await _roleManager.GetRoleByIdAsync(input.Role.Id.Value);
             role.DisplayName = input.Role.DisplayName;
             role.IsDefault = input.Role.IsDefault;
+            role.Name = input.Role.Name;
 
             await UpdateGrantedPermissionsAsync(role, input.GrantedPermissionNames);
         }
