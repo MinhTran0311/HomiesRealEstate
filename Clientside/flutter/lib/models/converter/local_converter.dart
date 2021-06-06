ee5c911bb5f5975674165dbb638c31e644777160
MinhTran0311 <50918867+MinhTran0311@users.noreply.github.com>
2021-06-06 14:39:22 +0700

    done permission

--- Clientside/flutter/lib/models/converter/local_converter.dart
+++ Clientside/flutter/lib/models/converter/local_converter.dart
@@ -17,6 +17,19 @@ String priceFormat(double price){
   return price.toString() + ' ' + array[position];
 }
 
+String priceFormatForFilter(String inputPrice){
+  if (inputPrice.isEmpty) return "";
+  var price = double.parse(inputPrice);
+  int position = 0;
+  for (position=0; position<6; position++){
+    if (price.abs() < pow(1000,position)*1000){
+      break;
+    }
+  }
+  price /= pow(1000,position);
+  return price.toString();
+}
+
 Image imageFromBase64String(String base64String) {
   Uint8List bytes = base64.decode(base64String);
   return Image.memory(bytes);
