0759346bd78e916d83eb29d3e41a815ad3d16dea
ictnmd <ictnmd@gmail.com>
2021-06-15 16:11:10 +0700

    BaiDang-DoUuTien

---
+++ Serverside/aspnet-core/src/Homies.RealEstate.EntityFrameworkCore/Migrations/20210615090729_BaiDang-DoUTie.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Homies.RealEstate.Migrations
+{
+    public partial class BaiDangDoUTie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DoUuTien",
+                table: "BaiDangs",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DoUuTien",
+                table: "BaiDangs");
+        }
+    }
+}
