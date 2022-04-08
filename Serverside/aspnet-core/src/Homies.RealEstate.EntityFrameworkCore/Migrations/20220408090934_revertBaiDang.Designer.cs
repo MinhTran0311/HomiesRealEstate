13371543df215b7970faaa47b61092c854d45281
ictnmd <ictnmd@gmail.com>
2022-04-08 16:26:07 +0700

    revertBaiDang

--- Serverside/aspnet-core/src/Homies.RealEstate.EntityFrameworkCore/Migrations/20220408090934_revertBaiDang.Designer.cs
+++ Serverside/aspnet-core/src/Homies.RealEstate.EntityFrameworkCore/Migrations/20220408090934_revertBaiDang.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Homies.RealEstate.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Homies.RealEstate.Migrations
 {
     [DbContext(typeof(RealEstateDbContext))]
-    partial class RealEstateDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220408090934_revertBaiDang")]
+    partial class revertBaiDang
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Serverside/aspnet-core/src/Homies.RealEstate.EntityFrameworkCore/Migrations/20220408090934_revertBaiDang.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Homies.RealEstate.Migrations
+{
+    public partial class revertBaiDang : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ToaDoY",
+                table: "BaiDangs",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ToaDoX",
+                table: "BaiDangs",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "DienTich",
+                table: "BaiDangs",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DoUuTien",
+                table: "BaiDangs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FeaturedImage",
+                table: "BaiDangs",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Gia",
+                table: "BaiDangs",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DienTich",
+                table: "BaiDangs");
+
+            migrationBuilder.DropColumn(
+                name: "DoUuTien",
+                table: "BaiDangs");
+
+            migrationBuilder.DropColumn(
+                name: "FeaturedImage",
+                table: "BaiDangs");
+
+            migrationBuilder.DropColumn(
+                name: "Gia",
+                table: "BaiDangs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ToaDoY",
+                table: "BaiDangs",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ToaDoX",
+                table: "BaiDangs",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+        }
+    }
+}
